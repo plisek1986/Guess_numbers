d04cdbfbaba92c05b72a208dacd9528c96a7740d
plisek1986 <pawel.lisek.fp@gmail.com>
2021-02-16 21:12:59 +0100

    First code lines with docstrings and user_choice function

---
+++ Exercise3_1.py
@@ -0,0 +1,8 @@
+def user_choice():
+    """
+    This functions is focused on the user and his feedback to computer's number
+
+    Returns: Answer from the user (str)
+
+    """
+    available_answers = ['too small', 'too big', 'you win']
